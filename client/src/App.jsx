2c5ccc96a0db1afe3c59118df6f57993bf4cd4fd
RanaInCode <ar4930705@gmail.com>
2025-07-09 23:20:21 +0530

    Updating the basename in BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import './App.css'
 function App() {
   return (
     <>
-       <BrowserRouter>
+       <BrowserRouter basename="/Narrify">
         <MainContent />
     </BrowserRouter> 
     </>
